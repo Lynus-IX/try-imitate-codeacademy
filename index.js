b72d770a4cbc7ff4742c2ed443ceccbeb6c226c8
Willy <willys.manroe@gmail.com>
2022-07-09 22:54:02 +0700

    add console function

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 // retrieve elements
 const runCodeBtn = document.querySelector(".editor__run");
 const resetCodeBtn = document.querySelector(".editor__reset");
+const consoleDiv = document.querySelector(".editor__console");
 
 var codeEditor = ace.edit("editorCode");
+var defaultCode = `console.log("Hello World");`;
+let consoleMessage = "";
 
 var editorLib = {
+  printConsole() {
+    const newLine = document.createElement("div");
+    newLine.append(`> ${consoleMessage}`);
+    consoleDiv.append(newLine);
+  },
+  clearConsole() {
+    consoleDiv.remove(consoleDiv.children);
+  },
   init() {
     codeEditor.setTheme("ace/theme/cloud9_night_low_color");
-    codeEditor.session.setMode("ace/mode/golang");
+    codeEditor.session.setMode("ace/mode/javascript");
+    codeEditor.setValue(defaultCode);
   },
 };
 
-runCodeBtn.addEventListener("click", () => {});
+runCodeBtn.addEventListener("click", () => {
+  const userCode = codeEditor.getValue();
+  try {
+    new Function(userCode)();
+  } catch (err) {
+    console.log(err);
+  }
+  editorLib.printConsole();
+});
 
 resetCodeBtn.addEventListener("click", () => {
-  codeEditor.setValue("");
+  codeEditor.setValue(defaultCode);
+  editorLib.clearConsole();
 });
 
 editorLib.init();
